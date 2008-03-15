b1801764da98a8a8cd07e575a607100d0e5a3bce
Toby de Havilland <toby.de.havilland@puremvc.org>
2008-03-15 14:37:11 +0000

    new tag

--- trunk/build/lib/puremvc/patterns/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
- PureMVC Python Port by Toby de Havilland <toby.de.havilland@puremvc.org> 
- PureMVC - Copyright(c) 2006-08 Futurescale, Inc., Some rights reserved. 
- Your reuse is governed by the Creative Commons Attribution 3.0 License 
-"""
