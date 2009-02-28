8a2548b7b573f6d45c3b8b360ff222743726f351
Toby de Havilland <toby.de.havilland@puremvc.org>
2009-02-28 13:40:55 +0000

    Updated docs and fabfile

--- fabfile.py
+++ fabfile.py
@@ -5,17 +5,17 @@ def clean():
     'remove local .pyc files'
 
     local("find ./src/ -name '*.pyc' -exec rm -rf {} \;")    
-    local("find ./Unit\ Tests/ -name '*.pyc' -exec rm -rf {} \;")    
+    local("find ./unittest/ -name '*.pyc' -exec rm -rf {} \;")    
 
 def docs():
     'Build epydocs'
     setup()
 
-    local("epydoc --html src/puremvc -o epydoc/")
+    local("epydoc --html ./src/puremvc -o ./epydoc/")
 
 
 def test():
     'Run unit tests'
     setup()
 
-    local("python Unit\ Tests/main.py")
+    local("python ./unittest/main.py")
