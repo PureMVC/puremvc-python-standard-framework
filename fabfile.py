db0ba3faa70885b67b43ccf213f8a01bc5e630f1
Toby de Havilland <toby.de.havilland@puremvc.org>
2009-02-28 13:43:07 +0000

    Tagging 2.0.4

--- fabfile.py
+++ fabfile.py
@@ -1,28 +1,21 @@
-"""
-Usage:
-
-fab test
-fab docs
-"""
-
 def setup():
     pass
 
 def clean():
     'remove local .pyc files'
+
     local("find ./src/ -name '*.pyc' -exec rm -rf {} \;")    
-    local("find ./Unit\ Tests/ -name '*.pyc' -exec rm -rf {} \;")    
+    local("find ./unittest/ -name '*.pyc' -exec rm -rf {} \;")    
 
 def docs():
     'Build epydocs'
     setup()
 
-    local("rm -rf ./epydoc")
-    local("epydoc --html src/puremvc -o epydoc/")
+    local("epydoc --html ./src/puremvc -o ./epydoc/")
 
 
 def test():
     'Run unit tests'
     setup()
 
-    local("python Unit\ Tests/main.py")
+    local("python ./unittest/main.py")
