60315100b1a8122eed85ee3d9db821069c367b31
Daniele Esposti <expo@expobrain.net>
2012-10-11 00:14:25 +0100

    Missing inheritance from INotifier for facade, command and mediator interfaces

--- src/puremvc/interfaces.py
+++ src/puremvc/interfaces.py
@@ -4,7 +4,43 @@
  Your reuse is governed by the Creative Commons Attribution 3.0 License
 """
 
-class ICommand(object):
+
+class INotifier(object):
+    """
+    The interface definition for a PureMVC Notifier.
+
+    C{MacroCommand, Command, Mediator} and C{Proxy}
+    all have a need to send C{Notifications}.
+
+    The C{INotifier} interface provides a common method called
+    C{sendNotification} that relieves implementation code of
+    the necessity to actually construct C{Notifications}.
+
+    The C{Notifier} class, which all of the above mentioned classes
+    extend, also provides an initialized reference to the C{Facade}
+    Singleton, which is required for the convenience method
+    for sending C{Notifications}, but also eases implementation as these
+    classes have frequent C{Facade} interactions and usually require
+    access to the facade anyway.
+
+    @see: L{IFacade<puremvc.interfaces.IFacade>}
+    @see: L{INotification<puremvc.interfaces.INotification>}
+    """
+    def sendNotification(self, notificationName, body=None, noteType=None):
+        """
+        Send a C{INotification}.
+
+        Convenience method to prevent having to construct new
+        notification instances in our implementation code.
+
+        @param notificationName: the name of the notification to send
+        @param body: the body of the notification (optional)
+        @param type: the type of the notification (optional)
+        """
+        raise NotImplemented(self)
+
+
+class ICommand(INotifier):
     """
     The interface definition for a PureMVC Command.
 
@@ -81,7 +117,7 @@ class IController(object):
         raise NotImplemented
 
 
-class IFacade(object):
+class IFacade(INotifier):
     """
     The interface definition for a PureMVC Facade.
 
@@ -201,7 +237,7 @@ class IFacade(object):
         raise NotImplemented
 
 
-class IMediator(object):
+class IMediator(INotifier):
     """
     The interface definition for a PureMVC Mediator.
 
@@ -403,41 +439,6 @@ class INotification(object):
         raise NotImplemented
 
 
-class INotifier(object):
-    """
-    The interface definition for a PureMVC Notifier.
-
-    C{MacroCommand, Command, Mediator} and C{Proxy}
-    all have a need to send C{Notifications}.
-
-    The C{INotifier} interface provides a common method called
-    C{sendNotification} that relieves implementation code of
-    the necessity to actually construct C{Notifications}.
-
-    The C{Notifier} class, which all of the above mentioned classes
-    extend, also provides an initialized reference to the C{Facade}
-    Singleton, which is required for the convenience method
-    for sending C{Notifications}, but also eases implementation as these
-    classes have frequent C{Facade} interactions and usually require
-    access to the facade anyway.
-
-    @see: L{IFacade<puremvc.interfaces.IFacade>}
-    @see: L{INotification<puremvc.interfaces.INotification>}
-    """
-    def sendNotification(self, notificationName, body = None, type = None):
-        """
-        Send a C{INotification}.
-
-        Convenience method to prevent having to construct new
-        notification instances in our implementation code.
-
-        @param notificationName: the name of the notification to send
-        @param body: the body of the notification (optional)
-        @param type: the type of the notification (optional)
-        """
-        raise NotImplemented
-
-
 class IObserver(object):
     """
     The interface definition for a PureMVC Observer.
@@ -515,7 +516,7 @@ class IObserver(object):
         raise NotImplemented
 
 
-class IProxy(object):
+class IProxy(INotifier):
     """
     The interface definition for a PureMVC Proxy.
 
