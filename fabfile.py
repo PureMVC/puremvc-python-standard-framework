978b687da4c665198cb34897859af88d031d0890
Daniele Esposti <expo@expobrain.net>
2012-10-11 00:12:21 +0100

    Converted tabs into spaces and removed trailing spaces

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,15 @@
 from fabric.api import *
 
 def install():
-    
+
     local("python ./setup.py clean")
     local("python ./setup.py install")
 
 def clean():
     'remove local .pyc files'
 
-    local("find ./src/ -name '*.pyc' -exec rm -rf {} \;")    
-    local("find ./tests/ -name '*.pyc' -exec rm -rf {} \;")    
+    local("find ./src/ -name '*.pyc' -exec rm -rf {} \;")
+    local("find ./tests/ -name '*.pyc' -exec rm -rf {} \;")
 
 def docs():
     'Build docs'
