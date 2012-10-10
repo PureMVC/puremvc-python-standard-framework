beec2ec47a06c179867533536a097590d1f32e39
Daniele Esposti <expo@expobrain.net>
2012-10-11 00:13:11 +0100

    Fixed 'test' task in fabric file

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,10 @@
 from fabric.api import *
 
-def install():
 
+def install():
     local("python ./setup.py clean")
     local("python ./setup.py install")
+
 
 def clean():
     'remove local .pyc files'
@@ -11,14 +12,14 @@ def clean():
     local("find ./src/ -name '*.pyc' -exec rm -rf {} \;")
     local("find ./tests/ -name '*.pyc' -exec rm -rf {} \;")
 
+
 def docs():
     'Build docs'
 
     local("rm -rf ./docs/*")
     local("epydoc --html --name 'PureMVC Python' ./src/puremvc -o ./docs/")
 
+
 def test():
     'Run unit tests'
-    setup()
-
     local("python ./tests/main.py")
